9a4dd5f1ad6b84e3ad7e582c2a84dbab5bfaa542
violettadudiak0210 <violettadudiak0210@gmail.com>
2025-02-21 19:32:44 +0000

    update

--- js/task-6.js
+++ js/task-6.js
@@ -1,41 +1,50 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-const input = document.querySelector("#controls input");
-const createBtn = document.querySelector("[data-create]");
-const destroyBtn = document.querySelector("[data-destroy]");
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const createBtn = controls.querySelector("[data-create]");
+const destroyBtn = controls.querySelector("[data-destroy]");
 const boxes = document.querySelector("#boxes");
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
-
 function createBoxes(amount) {
-  boxes.innerHTML = "";
+  
+  destroyBoxes();
+
+  const fragment = document.createDocumentFragment();
   let size = 30;
-  const elements = [];
 
   for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    elements.push(box);
-    size += 10;
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.margin = "5px";
+    div.style.borderRadius = "4px";
+    fragment.appendChild(div);
+    size += 10; 
   }
 
-  boxes.append(...elements);
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
 }
 
 createBtn.addEventListener("click", () => {
-  const amount = Number(input.value);
-  if (amount >= 1 && amount <= 100) createBoxes(amount);
-  input.value = "";
+  const amount = Number(input.value.trim());
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100!");
+  }
 });
 
-destroyBtn.addEventListener("click", () => {
-  boxes.innerHTML = "";
-});
+
+destroyBtn.addEventListener("click", destroyBoxes);
